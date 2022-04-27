ebafd4cd1175f15a3dd654fab5710481b48e101a
Sarah <sarahjonet.pro@gmail.com>
2022-04-27 17:30:47 +0200

    ajout dossiers et fichiers

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"rel="stylesheet"/>
+<!-- MDB -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css"rel="stylesheet"/>
+<link href="assets/sass/style.css"rel="stylesheet"/>
+</head>
+<body>
+    <!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js">
+</script>
+
+<footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
+<!-- Grid container -->
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(13, 129, 135, 1);">
+&copy 2022 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">sarahjonet.pro@gmail.com</a>
+    <img src="assets\image\github (1).png" alt="logo_github" width="50" height="auto">
+    <img src ="assets\image\linkedin.png" alt="logo_linkedin"width="50" height="auto">
+</div>
+<!-- Copyright -->
+</footer>
+</body>
+</html>
